9b31be3c9d342d6eec762299f20ebe1e9d94412d
Alexandreiut <131276833+Alexandreiut@users.noreply.github.com>
2025-01-27 17:39:55 +0100

    Initialisation des méthodes

---
+++ app/src/engine/solver.py
@@ -0,0 +1,20 @@
+from app.src.engine.grille import Grille
+class Solver
+
+  """
+  classe exploitant l'algorithme A*
+  avec 3 types de calcul de l'heristique
+  - Dijkstra
+  - Ville ou quadrillage
+  - Pythagore
+  """
+
+  def solve_grid_pythagore(grid : Grille) -> Grille
+      """
+      """
+  def solve_grid_ville(grid : Grille) -> Grille
+      """
+      """
+  def solve_grid_pythagore(grid : Grille) -> Grille
+      """
+      """
